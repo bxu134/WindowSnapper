b2ed95d229d909a35e60de7e10da92203fc5b241
Ben Xu <bxu.nc134@gmail.com>
2025-11-24 13:08:09 -0500

    Initial Commit

---
+++ WindowSnapper/WindowSnapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowSnapperApp.swift
+//  WindowSnapper
+//
+//  Created by Ben Xu on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct WindowSnapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WindowSnapperTests/WindowSnapperTests.swift
@@ -0,0 +1,17 @@
+//
+//  WindowSnapperTests.swift
+//  WindowSnapperTests
+//
+//  Created by Ben Xu on 11/24/25.
+//
+
+import Testing
+@testable import WindowSnapper
+
+struct WindowSnapperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
